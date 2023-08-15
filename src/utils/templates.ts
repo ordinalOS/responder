f42f74a93f7a17f905973da6a514fe8d1ca3d74e
cloutcoin <ordinalos@proton.me>
2023-08-14 19:27:17 -0700

    ocl

--- src/utils/templates.ts
+++ src/utils/templates.ts
@@ -5,42 +5,50 @@ interface Template {
 }
 
 const defaultTemplates: Template[] = [
+  // ... Other templates
   {
-    title: 'Prise de contact',
-    body: `<p>Hello [name],<br><br>
-      Merci pour cette prise de contact. Pouvez-vous m’en dire un peu plus sur cette mission ?<br><br>
-      Pour me présenter rapidement, je travaille en freelance depuis 4 ans dans l’accompagnement d’agences web pour de l’intégration sur les principaux CMS du marché mais aussi en tant que développeur fullstack pour la création d’applications web et mobile.<br><br>
-      Mes clients travaillent principalement avec WordPress mais j’ai également eu l’occasion d’être missionné sur des projets Drupal, PrestaShop ou des bases customs construites avec Node.js, Symfony et Laravel.<br><br>
-      Côté front, je maintiens un laboratoire qui me tient actuellement de portfolio et sur lequel j’aime à pousser les limites de ce qu’il est possible de faire dans un navigateur : https://lab.julienverneaut.com. Si je travaille principalement avec les différents systèmes de templating propres aux CMS évoqués précédemment, je suis également à l’aise avec React et Vue.js pour le développement d’applications ou de fonctionnalités plus complexes.<br><br>
-      La plupart des projets réalisés pour mes clients étant protégés par un NDA, je n’ai pas l’autorisation de les partager sur mes réseaux publics. Je pourrais néanmoins vous envoyer quelques références par message si mon profil venait à attirer davantage votre attention.<br><br>
-      Voici également un lien vers mon GtiHub sur lequel je maintiens quelques projets personnels et qui offre un aperçu de mes pratiques en matière de développement : https://github.com/jverneaut<br><br>
-      Merci encore pour prise de contact, je reste à disposition si vous avez besoin d’informations complémentaires.
-    </p>`,
-    tags: ['linkedin'],
+    title: 'Dublin Core Metadata',
+    body: `
+    &lt;metadata xmlns:ocl="http://purl.org/dc/elements/1.1/"&gt;<br>
+        &lt;ocl:title&gt;<span class="shortcode">[ocl:title]</span>&lt;/ocl:title&gt;<br>
+        &lt;ocl:creator&gt;<span class="shortcode">[ocl:creator]</span>&lt;/ocl:creator&gt;<br>
+        &lt;ocl:subject&gt;<span class="shortcode">[ocl:subject]</span>&lt;/ocl:subject&gt;<br>
+        &lt;ocl:description&gt;<span class="shortcode">[ocl:description]</span>&lt;/ocl:description&gt;<br>
+        &lt;ocl:publisher&gt;<span class="shortcode">[ocl:publisher]</span>&lt;/ocl:publisher&gt;<br>
+        &lt;ocl:contributor&gt;<span class="shortcode">[ocl:contributor]</span>&lt;/ocl:contributor&gt;<br>
+        &lt;ocl:date&gt;<span class="shortcode">[ocl:date]</span>&lt;/ocl:date&gt;<br>
+        &lt;ocl:type&gt;<span class="shortcode">[ocl:type]</span>&lt;/ocl:type&gt;<br>
+        &lt;ocl:format&gt;<span class="shortcode">[ocl:format]</span>&lt;/ocl:format&gt;<br>
+        &lt;ocl:identifier&gt;<span class="shortcode">[ocl:identifier]</span>&lt;/ocl:identifier&gt;<br>
+        &lt;ocl:source&gt;<span class="shortcode">[ocl:source]</span>&lt;/ocl:source&gt;<br>
+        &lt;ocl:language&gt;<span class="shortcode">[ocl:language]</span>&lt;/ocl:language&gt;<br>
+        &lt;ocl:relation&gt;<span class="shortcode">[ocl:relation]</span>&lt;/ocl:relation&gt;<br>
+        &lt;ocl:rights&gt;<span class="shortcode">[ocl:rights]</span>&lt;/ocl:rights&gt;<br>
+        &lt;ocl:coverage&gt;<span class="shortcode">[ocl:coverage]</span>&lt;/ocl:coverage&gt;<br>
+    &lt;/metadata&gt;<br>
+    `,
+    tags: ['dublin-core', 'metadata'],
   },
   {
-    title: 'Réponse favorable proposition de poste',
-    body: `<p>
-      Bonjour [interlocuteur],<br><br>
-      Merci de m'avoir contacté au sujet de votre offre de [nom de l'offre].
-      Je suis actuellement à la recherche de nouvelles opportunités et je souhaiterai en discuter avec vous.<br><br>
-      Seriez vous disponible pour un rendez-vous [date] ?<br><br>
-      [formule de politesse],<br>
-      [signature]
-    </p>`,
-    tags: ['positif', 'linkedin', 'recruteur', 'oui', 'entretien'],
-  },
-  {
-    title: 'Réponse défavorable proposition de poste',
-    body: `<p>
-      Bonjour [interlocuteur],<br><br>
-      Merci de m'avoir contacté au sujet de votre offre de [nom de l'offre].
-      Je suis actuellement en mission jusqu'à [date de fin de mission] et ne pourrait donc pas y répondre favorablement.<br><br>
-      En revanche, je reste à l'écoute de toute proposition future de votre part.<br><br>
-      [formule de politesse],<br>
-      [signature]
-    </p>`,
-    tags: ['négatif', 'linkedin', 'recruteur', 'oui', 'entretien'],
+    title: 'ID3 Music Metadata',
+    body: `
+    <div class="content" contenteditable="true">
+        &lt;metadata&gt;<br>
+            &lt;ocl:title&gt;<span class="shortcode">[song:title]</span>&lt;/title&gt;<br>
+            &lt;ocl:artist&gt;<span class="shortcode">[song:artist]</span>&lt;/artist&gt;<br>
+            &lt;ocl:album&gt;<span class="shortcode">[song:album]</span>&lt;/album&gt;<br>
+            &lt;ocl:genre&gt;<span class="shortcode">[song:genre]</span>&lt;/genre&gt;<br>
+            &lt;ocl:release_date&gt;<span class="shortcode">[song:release_date]</span>&lt;/release_date&gt;<br>
+            &lt;ocl:duration&gt;<span class="shortcode">[song:duration]</span>&lt;/duration&gt;<br>
+            &lt;ocl:explicit&gt;<span class="shortcode">[song:explicit]</span>&lt;/explicit&gt;<br>
+            &lt;ocl:isrc&gt;<span class="shortcode">[song:isrc]</span>&lt;/isrc&gt;<br>
+            &lt;ocl:upc&gt;<span class="shortcode">[song:upc]</span>&lt;/upc&gt;<br>
+            &lt;ocl:label&gt;<span class="shortcode">[song:label]</span>&lt;/label&gt;<br>
+            &lt;ocl:external_urls&gt;<span class="shortcode">[song:external_urls]</span>&lt;/external_urls&gt;<br>
+        &lt;/metadata&gt;<br>
+    </div>
+    `,
+    tags: ['ID3', 'music', 'metadata'],
   },
 ];
 
